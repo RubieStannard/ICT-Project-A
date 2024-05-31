cee077f442cf8431fe642ca29486347d608936a1
RubieStannard <126431815+RubieStannard@users.noreply.github.com>
2024-05-31 14:50:22 +1000

    Delete Aged Care Website directory

--- Aged Care Website/Admin/My Version/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "feenix-mariadb.swin.edu.au";
-$user = ""; // your user name - sID
-$pswd = ""; // your password d(date of birth – ddmmyy)
-$dbnm = ""; // your database - sID_db
-?>
--- Aged Care Website/Admin/employeeindex.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //
-    $conn = mysqli_connect("localhost", "root", "", "members") or die("Failed!".mysqli_error());
-    mysqli_query($conn,"set names utf8");//
-    $result = mysqli_query($conn,"select * from employee order by id desc");	// 执
-    $number = mysqli_num_rows($result); //获取查询条数
-    include_once('viewEmployeelists.html');         //
-
